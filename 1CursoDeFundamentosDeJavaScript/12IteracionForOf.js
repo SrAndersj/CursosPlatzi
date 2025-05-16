b695ea01c0d69d8e7514af9a63fef5829dcb2b66
PcAudio <byandersj@gmail.com>
2025-05-16 16:01:28 -0500

    Iteración con for-of en JavaScript para arrays y strings

---
+++ 1CursoDeFundamentosDeJavaScript/12IteracionForOf.js
@@ -0,0 +1,15 @@
+/*
+
+for of arrays, strings [algo]
+
+for (varible of objeto) {
+  codigo 
+}
+
+*/
+
+let canasta = ["manzana", "pera", "naranja", "uva"];
+
+for (fruta of canasta) {
+  console.log(fruta);
+}

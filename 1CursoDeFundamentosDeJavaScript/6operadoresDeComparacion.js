fd1f91ec2699ec6ecbf5732e572edb4fa81ce904
PcAudio <byandersj@gmail.com>
2025-05-16 14:13:32 -0500

    peradores de Comparación en JavaScript: Igualdad y Desigualdad

---
+++ 1CursoDeFundamentosDeJavaScript/6operadoresDeComparacion.js
@@ -0,0 +1,21 @@
+/*
+
+Operadores de comparaciÃ³n
+
+*/
+
+== 
+===   // este compara que sean iguales en valor y en tipo
+!=  // diferente de
+!==  // tambien compara que es diferente en el valor y tipo de dato
+>
+< 
+>= 
+<= 
+
+const a = 10; 
+const b = 20; 
+const c = "10"
+
+a == b; 
+

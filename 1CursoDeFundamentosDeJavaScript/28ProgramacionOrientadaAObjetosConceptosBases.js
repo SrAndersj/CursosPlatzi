88e5762214137a33241d7b00690a70c99c51c04d
PcAudio <byandersj@gmail.com>
2025-05-20 19:58:53 -0500

    Programación Orientada a Objetos en JavaScript: Conceptos y Práctica

---
+++ 1CursoDeFundamentosDeJavaScript/28ProgramacionOrientadaAObjetosConceptosBases.js
@@ -0,0 +1,28 @@
+/*
+
+estructura de datos
+
+key / value 
+
+objeto {
+  propiedad: valor, 
+  propiedad: valor, 
+  propiedad: valor 
+  Metodos()
+}
+
+*/
+
+const persona = {
+  nombre: "John",
+  edad: 30,
+  direccion: {
+    calle: "Av Insurgentre 187",
+    ciudad: "CDMX",
+  },
+  saludar() {
+    console.log(`hola, mi nombre es ${persona.nombre}`);
+  },
+};
+
+/

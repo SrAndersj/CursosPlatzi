0965be3a0b5375636bd68763e1b8e89deeae1b19
PcAudio <byandersj@gmail.com>
2025-05-28 09:53:19 -0500

    Clases en JavaScript: Sintaxis y Uso Práctico

--- 3CursoBasicoProgramacionOrientadaAObjetosJavaScript/main.js
+++ 3CursoBasicoProgramacionOrientadaAObjetosJavaScript/main.js
@@ -27,3 +27,42 @@ const juanita = new Student("Juanita", 15, [
   "Curso de creación de videojuegos",
   "Curso Práctico de HTML y CSS",
 ]);
+
+//clases
+
+//prototipos con la sintexis clases
+
+//cuando usamos {} dentro del constructor
+//nos permite enviarla la info  con cosas faltantes
+//solo senalando lo que ingresamos , y podemos dejar cosas
+// por defecto como cursosAprobados = [] con array vacio
+
+class Student2 {
+  constructor({
+    name,
+
+    age,
+
+    email,
+    facebook,
+    twitter,
+    cursosAprobados = [],
+  }) {
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+    this.email = email;
+    this.facebook = facebook;
+    this.twitter = twitter;
+  }
+
+  aprobarCurso(nuevoCursito) {
+    this.cursosAprobados.push(nuevoCursito);
+  }
+}
+
+const miguelito = new Student2({
+  name: "Miguelito",
+  age: 28,
+  email: "miguelito@platzi.com",
+});

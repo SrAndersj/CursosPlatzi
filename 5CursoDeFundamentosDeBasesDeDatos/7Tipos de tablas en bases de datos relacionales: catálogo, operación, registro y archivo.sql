2e07261a57e0a18cd4b66ae5a92afbb99c40222c
PcAudio <byandersj@gmail.com>
2025-09-07 11:39:06 -0500

    Tipos de tablas en bases de datos relacionales: catálogo, operación, registro y archivo

---
+++ 5CursoDeFundamentosDeBasesDeDatos/7Tipos de tablas en bases de datos relacionales: catálogo, operación, registro y archivo.sql
@@ -0,0 +1,21 @@
+-- tipos de tablas
+
+-- catálogo
+-- los elementos que se van a mostrar en la app , tabla que se modifica poco 
+
+
+-- operación
+
+--  registramos todo lo que nuestro negocio va haciendo como ventas
+
+
+
+-- registro
+--   es casi como la de operacion , para datos que cambian
+-- por ejemplo el precio de venta de una botella de agua puede cambiar constantemente
+
+
+-- archivo 
+-- es dejar almacenado todas las ejecuciones con permisos especiales
+-- que tiene un usuario. pierde normalizacion pero  facilita datos historicos suaves 
+
